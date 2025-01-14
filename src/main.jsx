9173ae3c28dca61b06c1db902ed93f90dc71b6b4
RomanTuras <salden.com@gmail.com>
2025-01-14 10:08:06 +0200

    Add state

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import 'modern-normalize';
-import './index.css'
-import App from './components/App/App.jsx'
-import {BrowserRouter} from "react-router-dom";
+import './index.css';
+import App from './components/App/App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store, { persistor } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <BrowserRouter>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
           <App />
-      </BrowserRouter>
-  </StrictMode>,
-)
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </StrictMode>
+);
--- src/pages/CamperPage/CamperPage.jsx
+++ src/pages/CamperPage/CamperPage.jsx
@@ -1,5 +1,6 @@
 const CamperPage = () => {
-    return <p>Camper Page</p>
-}
+  //TODO: getting ID from query params and get through useEffect camper by ID 
+  return <p>Camper Page</p>;
+};
 
 export default CamperPage;
