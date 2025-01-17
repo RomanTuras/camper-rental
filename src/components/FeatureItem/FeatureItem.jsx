bd256b236c1146d431772a69f3eaba749677d934
RomanTuras <salden.com@gmail.com>
2025-01-17 14:43:59 +0200

    Change icons paths

--- src/components/FeatureItem/FeatureItem.jsx
+++ src/components/FeatureItem/FeatureItem.jsx
@@ -1,13 +1,13 @@
-import AC from '../src/assets/images/icons/AC.svg';
-import TV from '/src/assets/images/icons/TV.svg';
-import kitchen from '/src/assets/images/icons/kitchen.svg';
-import automatic from '/src/assets/images/icons/automatic.svg';
-import bathroom from '/src/assets/images/icons/bathroom.svg';
-import radio from '/src/assets/images/icons/radio.svg';
-import refrigerator from '/src/assets/images/icons/refrigerator.svg';
-import microwave from '/src/assets/images/icons/microwave.svg';
-import gas from '/src/assets/images/icons/gas.svg';
-import water from '/src/assets/images/icons/water.svg';
+import AC from '../../assets/images/icons/AC.svg';
+import TV from '../../assets/images/icons/TV.svg';
+import kitchen from '../../assets/images/icons/kitchen.svg';
+import automatic from '../../assets/images/icons/automatic.svg';
+import bathroom from '../../assets/images/icons/bathroom.svg';
+import radio from '../../assets/images/icons/radio.svg';
+import refrigerator from '../../assets/images/icons/refrigerator.svg';
+import microwave from '../../assets/images/icons/microwave.svg';
+import gas from '../../assets/images/icons/gas.svg';
+import water from '../../assets/images/icons/water.svg';
 
 const icons = {
   AC,
