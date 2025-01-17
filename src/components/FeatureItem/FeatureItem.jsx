90908f25fd12583def60dde8b984ff4179097a41
RomanTuras <salden.com@gmail.com>
2025-01-17 14:16:54 +0200

    Add imports for using icons

--- src/components/FeatureItem/FeatureItem.jsx
+++ src/components/FeatureItem/FeatureItem.jsx
@@ -1,9 +1,32 @@
-const FeatureItem = ({feature}) => {
-  const getIconPathByName = name => `/src/assets/images/icons/${name}.svg`;
+import AC from '/src/assets/images/icons/AC.svg';
+import TV from '/src/assets/images/icons/TV.svg';
+import kitchen from '/src/assets/images/icons/kitchen.svg';
+import automatic from '/src/assets/images/icons/automatic.svg';
+import bathroom from '/src/assets/images/icons/bathroom.svg';
+import radio from '/src/assets/images/icons/radio.svg';
+import refrigerator from '/src/assets/images/icons/refrigerator.svg';
+import microwave from '/src/assets/images/icons/microwave.svg';
+import gas from '/src/assets/images/icons/gas.svg';
+import water from '/src/assets/images/icons/water.svg';
+
+const icons = {
+  AC,
+  TV,
+  kitchen,
+  automatic,
+  bathroom,
+  radio,
+  refrigerator,
+  microwave,
+  gas,
+  water,
+};
+
+const FeatureItem = ({ feature }) => {
 
   return (
     <li>
-      <img src={getIconPathByName(feature)} alt={feature} />
+      <img src={icons[feature]} alt={feature} />
       {feature}
     </li>
   );
